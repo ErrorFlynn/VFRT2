7c0a8e8f202184c4bc68f98bace6a5d7aa8f5b7d
ErrorFlynn <ErrorFlynn@users.noreply.github.com>
2018-04-09 19:49:31 -0400

    Add files via upload

--- VFRT2/fo4.h
+++ VFRT2/fo4.h
@@ -9,18 +9,26 @@
 // returns the FO4 path WITHOUT a trailing backslash
 wstring GetFO4Path(HWND hwnd = nullptr)
 {
+	const wstring exe{L"\\fallout4.exe"};
+	wstring fo4path{regkey{HKEY_LOCAL_MACHINE,
+		"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Steam App 377160"}.get_string("InstallLocation")};
+	if(fo4path.size() && FileExist(fo4path + exe)) return fo4path;
+
+	fo4path = regkey{HKEY_LOCAL_MACHINE, "SOFTWARE\\Bethesda Softworks\\Fallout4"}.get_string("Installed Path");
+	if(fo4path.size())
+	{
+		if(fo4path.back() == '\\') fo4path.pop_back();
+		if(FileExist(fo4path + exe)) return fo4path;
+	}
+
 	wstring steampath{regkey{HKEY_CURRENT_USER, "Software\\Valve\\Steam"}.get_string("SteamPath")};
 	if(steampath.size())
 	{
 		for(auto &c : steampath) if(c == '/') c = '\\';
 		if(steampath.back() != '\\') steampath.push_back('\\');
-		wstring fo4path{steampath + L"steamapps\\common\\Fallout 4"};
-		if(FileExist(fo4path)) return fo4path;
-		else
-		{
-			fo4path = steampath + L"steamapps\\common\\Fallout4";
-			if(FileExist(fo4path)) return fo4path;
-		}
+		fo4path = steampath + L"steamapps\\common\\Fallout 4";
+		if(FileExist(fo4path + exe)) return fo4path;
+
 		vector<string> folders;
 		ifstream f{steampath + L"config\\config.vdf"};
 		while(f.good())
@@ -42,21 +50,12 @@ wstring GetFO4Path(HWND hwnd = nullptr)
 			wstring wfolder;
 			mbtowc(folder, wfolder);
 			fo4path = wfolder + L"steamapps\\common\\Fallout 4";
-			if(FileExist(fo4path)) return fo4path;
-			fo4path = wfolder + L"steamapps\\common\\Fallout4";
-			if(FileExist(fo4path)) return fo4path;
+			if(FileExist(fo4path + exe)) return fo4path;
 		}
 	}
 
-	wstring fo4path{regkey{HKEY_LOCAL_MACHINE, 
-		"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Steam App 377160"}.get_string("InstallLocation")};
-	if(fo4path.size() && FileExist(fo4path)) return fo4path;
-
-	fo4path = regkey{HKEY_LOCAL_MACHINE, "SOFTWARE\\Bethesda Softworks\\Fallout4"}.get_string("Installed Path");
-	if(fo4path.size() && FileExist(fo4path)) return fo4path;
-
 	static wstring initfolder;
-	MessageBoxA(hwnd, "Failed to retrieve the Fallout 4 path from registry. Please locate the game folder manually. "
+	MessageBoxA(hwnd, "Failed to determine the Fallout 4 install location. Please locate the game folder manually. "
 		R"((For example: c:\Program Files (x86)\Steam\steamapps\common\Fallout 4))", "VFRT 2", MB_ICONEXCLAMATION);
 	folder_picker fp;
 	if(initfolder.size()) fp.initial_folder(initfolder);
